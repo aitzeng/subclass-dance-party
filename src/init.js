1382080e683c09c2cad84bda4e19a8bf43778405
aitzeng <aitzeng@gmail.com>
2023-04-04 17:01:53 -0700

    Updated init file

--- src/init.js
+++ src/init.js
@@ -22,44 +22,78 @@ $(document).ready(function() {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
     console.log(dancer);
     // console.log(dancer.$node)
     $('body').append(dancer.$node);
-    window.dancers.push(dancer.$node);
+    window.dancers.push(dancer);
     console.log(window.dancers);
   });
 
-  $(".LineUpLeft").on('click', function(event) {
+  $('.LineUpLeft').on('click', function(event) {
+    var top = 10;
     for (var i = 0; i < window.dancers.length; i++) {
       var dancer = window.dancers[i];
-      dancer.offSetLeft = 5;
+      top += 100;
+      dancer.setPosition.call(dancer, top, 10);
     }
   });
 
+  $('.findPartner').on('click', function(event) {
+    var top = 100;
+    var left = 100;
+    var left2 = 120;
+    for (var i = 0; i < window.dancers.length; i += 2) {
+      var dancer = window.dancers[i];
+      var dancer2 = window.dancers[i + 1];
+      top += 100;
+      dancer.setPosition.call(dancer, top, left);
+      dancer2.setPosition.call(dancer2, top, left2);
+    }
+  });
 
-  // ****** THIS IS THE ONE WE ARE ADDING BELOW *******
-  // $('.addPopDancerButton').on('click', function(event) {
-
-  //   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
-  //   // get the maker function for the kind of dancer we're supposed to make
-  //   var dancerMakerFunction = window[dancerMakerFunctionName];
-
-  //   // make a dancer with a random position
-
-  //   var dancer = new dancerMakerFunction(
-  //     $("body").height() * Math.random(),
-  //     $("body").width() * Math.random(),
-  //     Math.random() * 1000
-  //   );
-  //   $('body').append(dancer.$node);
+  // $('.dancer').on('mouseover', function(event) {
+  //   var style = {
+  //     // visibility: hidden;
+  //     display: none
+  //   };
+  //   $('.dancer').css(style);
   // });
-  // ******* END OF WHAT WE ADDED *************
 
 
 });
 
+
+
+
+
+// $('.findPartner').on('click', function(event) {
+//   var newList = [];
+//   var nodeMatch;
+//   var smallNum = 1000000000;
+
+//   for (var i = 0; i < window.dancers.length; i++) {
+//     var dancer = window.dancers[i];
+//     var currentLeft = dancer.$node.left;
+//     var currentTop = dancer.$node.top;
+//     console.log(dancer);
+
+//     for ( var j = i + 1; j < window.dancers.length; j++ ) {
+//       var closest;
+//       var nextDancer = window.dancer[j];
+//       var nextDancerTop = nextDancer.$node.top;
+//       var nextDancerLeft = nextDancer.$node.left;
+//       var distance = ((currentLeft - nextDancerLeft)**2 + (currentTop - nextDancerTop)**2)**(1/2);
+//       var count = mainLeft - secondDancer.left;
+//       count += maintop - secondDancer.top;
+//       if ( count < smallNum ) {
+//         nodeMatch = secondDancer;
+//       }
+//     }
+//     newList.push(nodeMatch);
+
+
+//   }
